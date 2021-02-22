c4e7e757fb70264b494d156cd9eb159d4205b803
Vyacheslav Dobrynin <dobrinin_v_j@ntik.ru>
2021-02-22 13:02:07 +0300

    added test

---
+++ src/test/java/guru/springframework/msscssm/services/PaymentServiceImplTest.java
@@ -0,0 +1,44 @@
+package guru.springframework.msscssm.services;
+
+import guru.springframework.msscssm.domain.Payment;
+import guru.springframework.msscssm.domain.PaymentEvent;
+import guru.springframework.msscssm.domain.PaymentState;
+import guru.springframework.msscssm.repository.PaymentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.statemachine.StateMachine;
+
+import java.math.BigDecimal;
+
+@SpringBootTest
+class PaymentServiceImplTest {
+
+    @Autowired
+    PaymentService paymentService;
+    @Autowired
+    PaymentRepository paymentRepository;
+
+    Payment payment;
+
+    @BeforeEach
+    void setUp() {
+        payment = Payment.builder()
+                .amount(BigDecimal.valueOf(12.99))
+                .build();
+    }
+
+    @Test
+    void preAuth() {
+        Payment savedPayment = paymentService.newPayment(this.payment);
+
+        StateMachine<PaymentState, PaymentEvent> stateMachine = paymentService.preAuth(savedPayment.getId());
+
+        Payment preAuthedPayment = paymentRepository.findById(savedPayment.getId()).orElse(null);
+
+        System.out.println(stateMachine.getState().getId());
+
+        System.out.println(preAuthedPayment);
+    }
+}
