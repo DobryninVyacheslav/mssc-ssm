f374f854c0e4da67a9f33866be25d18b38db23fd
Vyacheslav Dobrynin <dobrinin_v_j@ntik.ru>
2021-02-22 12:59:16 +0300

    added builder and toString

--- src/main/java/guru/springframework/msscssm/domain/Payment.java
+++ src/main/java/guru/springframework/msscssm/domain/Payment.java
@@ -1,9 +1,11 @@
 package guru.springframework.msscssm.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -14,6 +16,8 @@ import java.math.BigDecimal;
 
 @Getter
 @Setter
+@Builder
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
